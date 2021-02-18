9e38952d9b9f3534235e5dc5edd5a43a6e8b0b23
leolecrado <leocrapart@yahoo.fr>
2021-02-18 15:01:20 +0100

    1

--- api/index.py
+++ api/index.py
@@ -1,9 +1,37 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import mongo.index as mongo
 
 app = FastAPI()
+
+class WordItem(BaseModel):
+    word: str
+
 
 @app.get("/")
 def home():
 	return {
 		"weeeelcome": "its working 2"
 	}
+
+
+@app.get("/words")
+def getAllWords():
+    wordList = mongo.getAllWords()
+    return {
+        "words": wordList
+    }
+
+@app.post("/words")
+def insertWord(wordItem: WordItem):
+    insertedId = mongo.insertWord(wordItem)
+    return {
+        "_id": insertedId
+    }
+
+@app.delete("/words/{word}")
+def deleteWord(word: str):
+
+    deletedCount = mongo.deleteWord(word)
+    return {"deletedCount": deletedCount}
---
+++ mongo/index.py
@@ -0,0 +1,57 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://leocrapart:Dupr0pnet@cluster0.xbfcm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+db = client["voc_tool"]
+
+colWords = db["words"]
+
+## get   
+
+def getAllWords():
+    words = colWords.find()
+    wordList = list(map(lambda x: x["word"], words))
+    return wordList
+
+def getByWord(word):
+    query = {"word": word}
+    wordItem = colWords.find_one(query)
+    print(f"wordItem => {wordItem}")
+    
+    return {"word": wordItem["word"]}
+
+def getOneWord():
+    wordItem = colWords.find_one()
+    return wordItem
+
+## insert
+
+def insertWord(wordItem):
+    print(wordItem.dict())
+    res = colWords.insert_one(wordItem.dict())
+    insertedId = str(res.inserted_id)
+    return insertedId
+
+## update
+
+
+def deleteWord(word):
+    query = {
+        "word": word
+    }
+    print(query)
+    res = colWords.delete_many(query)
+    print(res)
+    return res.deleted_count
+
+## test
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
